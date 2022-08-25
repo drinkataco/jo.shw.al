a9bd1dce54624ddb83c438e8acfc53fbee75db7a
J J Walwyn <joshwalwyn@economist.com>
2022-08-25 19:08:52 +0100

    Started introducing linting with stylelint, a11y, and prettier

---
+++ stylelint.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  extends: ['stylelint-config-prettier'],
+  plugins: ['stylelint-prettier'],
+  rules: {
+    'at-rule-no-unknown': [
+      true,
+      {
+        ignoreAtRules: [
+          'tailwind',
+          'apply',
+          'variants',
+          'responsive',
+          'screen',
+        ],
+      },
+    ],
+    'declaration-block-trailing-semicolon': null,
+    'no-descending-specificity': null,
+    'prettier/prettier': true,
+  },
+};
